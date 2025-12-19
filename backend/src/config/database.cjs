ada95b13e860c4159a6535b68f54c58761e8caac
{aksa_carvalho} <{aksacarvalho43416@gmail.com}>
2025-12-19 07:28:50 -0300

    configuração do Sequelize e database

---
+++ backend/src/config/database.cjs
@@ -0,0 +1,16 @@
+module.exports = {
+  dialect: 'postgres',
+  host: 'localhost',
+  port: 5432,
+  username: 'admin',
+  password: '123456',
+  database: 'dev-burguer-db',
+  define: {
+    timestamps: true,
+    underscored: true,
+    underscoredAll: true,
+  },
+};
+
+// Usuários -> Nome, Email, Telefone, Data atualização
+// Nome, email, telefone, data_criação, DataCriação
