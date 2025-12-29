dbe9554637d686dd4580db83722ce749b4b2df58
{aksa_carvalho} <{aksacarvalho43416@gmail.com}>
2025-12-29 15:41:58 -0300

    Minha alteração local

--- backend/src/app/controllers/SessionController.js
+++ backend/src/app/controllers/SessionController.js
@@ -1,12 +1,12 @@
 import * as Yup from 'yup';
 import User from '../models/User.js';
 import bcrypt from 'bcrypt';
-
+import authConfig from './../../config/auth.js'
 class SessionController {
    async store(request, response) {
     const schema = Yup.object({
-      email: Yup.string().email(6).required(),
-      password: Yup.string().required(),
+      email: Yup.string().email().required(),
+      password: Yup.string().required().min(6),
     });
 
      const isValid = await schema.isValid(request.body, { 
@@ -14,8 +14,10 @@ class SessionController {
       strict: true 
     });
 
-       const emailOrPasswordIsIncorrect = () => {
-      response.status(400).json({ error: 'Email or password incorrect.' });
+      const emailOrPasswordIsIncorrect = () => { 
+     return response
+     .status(400)
+     .json({ error: 'Email or password incorrect.' });
     };
 
      if (!isValid){
@@ -34,14 +36,20 @@ class SessionController {
       emailOrPasswordIsIncorrect();
     }
 
-    const isPasswordCorrect = await bcrypt.compare(password,existingUser.password_hash,
+    const isPasswordCorrect = await bcrypt.compare(
+      password,
+      existingUser.password_hash,
   );
 
   if (!isPasswordCorrect) {
        emailOrPasswordIsIncorrect();
     }
+        
+     const token = jwt.sign({ id:  existingUser.id}, authConfig.secret, {
 
- return response.status(200).json({
+     }) 
+    
+     return response.status(200).json({
     id: existingUser.id,
       name: existingUser.name,
       email: existingUser.email,
