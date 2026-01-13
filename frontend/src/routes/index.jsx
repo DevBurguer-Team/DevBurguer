d546547e5c7a5ff0897895055ca0b650065c0483
{aksa_carvalho} <{aksacarvalho43416@gmail.com}>
2026-01-13 11:50:59 -0300

    Criando a tela de Register

--- frontend/src/routes/index.jsx
+++ frontend/src/routes/index.jsx
@@ -1,11 +1,16 @@
 import { createBrowserRouter }  from 'react-router-dom';
 
 import { Login } from '../containers/Login';
+import { Register } from '../containers/Register';
 
 export const router = createBrowserRouter([
   {
     path: '/',
     element: <Login />,
+  },
+   {
+    path: '/cadastro',
+    element: <Register />,
   },
   
 ]);
